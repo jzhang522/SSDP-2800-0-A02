9932417fc8d79b84944ccb7b4c4896d85dfb33dd
Jimmy <jzhang522@my.bcit.ca>
2025-12-06 21:01:52 -0800

    Cleanup

--- src/pages/Currency.jsx
+++ src/pages/Currency.jsx
@@ -1,13 +1,11 @@
 import CityInpuits from "../components/CityInputs";
-import WeatherCard from "../components/WeatherCard";
 
-export default function Home() {
+export default function Currency() {
     return (
         <>
             <div className="app-main">
                 <h2>💱 Currency Exchange</h2>
                 <CityInpuits></CityInpuits>
-                <WeatherCard></WeatherCard>
             </div>
         </>
     );
